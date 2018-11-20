b2e3afd54d1bf7e3f85c54a5f6d6d76ccfa3d9b5
Kelly Powell <s5128478@bournemouth.ac.uk>
2018-11-20 15:34:06 +0000

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Kelly Powell (s5128478) on 20/11/2018.
+//  Copyright © 2018 Kelly Powell (s5128478). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
