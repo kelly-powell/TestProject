fd97380f052f3e69106d1c73931ee3e082d5627e
Kelly Powell <s5128478@bournemouth.ac.uk>
2018-11-20 15:37:00 +0000

    removed comments

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  TestProject
-//
-//  Created by Kelly Powell (s5128478) on 20/11/2018.
-//  Copyright © 2018 Kelly Powell (s5128478). All rights reserved.
-//
 
 import UIKit
 
